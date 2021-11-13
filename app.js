1e43081a343a9611f852a5827589e98c98397835
Sylvie-B <unsolo@hotmail.fr>
2021-11-13 01:43:08 +0100

    début des calculs de points

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
-let container = document.querySelector("main");
+let main = document.querySelector("main");
+let target = document.querySelector('.target').getElementsByTagName('div');
 
-container.style.width = innerWidth + "px";
-container.style.height = innerHeight + "px";
+main.style.width = innerWidth + "px";
+main.style.height = innerHeight + "px";
 
+let score1 = 0;
+let score2 = 0;
+let score3 = 0;
+let volet = 0;
+let total = 0;
+let a = 0;
+for(let i = 0 ; i < target.length ; i++){
+    target[i].addEventListener('click', function (){
+        switch (a){
+            case 0 :
+                score1 = i;
+                a++
+                break;
+            case 1 :
+                score2 = i;
+                a++
+                break;
+            case 2 :
+                score3 = i;
+                console.log(score1, score2, score3);
+                volet = score1 + score2 + score3;
+                total += volet;
+                console.log("volet = " + volet);
+                console.log("total = " + total);
+                a = 0;
+                break;
+        }
+    })
+}
