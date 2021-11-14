1adc13cfbbc5fb4a0193045f873843005a71d8f2
Sylvie-B <unsolo@hotmail.fr>
2021-11-14 16:48:56 +0100

    work on function

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ let modal = document.querySelector('.modal');
 let final = document.getElementById('final');
 let go = document.querySelector('.fa-long-arrow-alt-right');
 
-
 main.style.width = innerWidth + "px";
 main.style.height = innerHeight + "px";
 
@@ -15,40 +14,49 @@ let arrow3 = 0;
 let score = 0;
 let total = 0;
 let a = 0;
-let recap = [];
-let turn = 0;
+let recap = [
+    [[0],[0, 0, 0]],
+    [[0],[0, 0, 0]],
+    [[0],[0, 0, 0]],
+    [[0],[0, 0, 0]],
+    [[0],[0, 0, 0]],
+];
 
+
+let turn = 0;
 
 for(let i = 0 ; i < target.length ; i++){
     target[i].addEventListener('click', function (){
         switch (a){
+            // arrow 1
             case 0 :
                 arrow1 = i;
                 a++
                 break;
+            // arrow 2
             case 1 :
                 arrow2 = i;
                 a++
                 break;
+            // arrow 3
             case 2 :
                 arrow3 = i;
                 score = arrow1 + arrow2 + arrow3;
-                total += score;
-                console.log(total);
+                // show data for validation
                 modal.style.display = "flex";
                 fly.innerHTML = arrow1 + " " + arrow2 + " " + arrow3 + "<br/>" + score + " points";
 
-
-                go.addEventListener('mouseup', function (){
-                    recap[turn] = [arrow1, arrow2, arrow3];
-                    console.log(recap);
-                    turn++;
-                    console.log(turn);
-                    modal.style.display = "none";
-                })
-                a = 0;
                 break;
         }
-
+        go.addEventListener('mouseup', function (){
+            total += score;
+            console.log("total : " + total);
+            recap[turn] = [["+"],[arrow1, arrow2, arrow3]];
+            modal.style.display = "none";
+            console.log("recap : " + recap);
+            turn++;
+            console.log("turn : " + turn);
+        })
+        a = 0;
     })
 }
