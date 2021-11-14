70d52c8b59d210fc905ae10f1a6ffa57e437cfb1
Sylvie-B <unsolo@hotmail.fr>
2021-11-14 21:47:58 +0100

    fly to flyNbr, turn to shoot

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let main = document.querySelector('main');
 let target = document.querySelector('.target').getElementsByTagName('div');
 console.log("target : " + target.length);
-let fly = document.getElementById('fly');
+let flyNbr = document.getElementById('flyNbr');
 let fullScreen = document.querySelector('.fullScreen');
 let final = document.getElementById('final');
 let go = document.querySelector('.fa-long-arrow-alt-right');
@@ -10,17 +10,17 @@ main.style.width = innerWidth + "px";
 main.style.height = innerHeight + "px";
 
 let totalScore = 0;
-let turn = 0;
+let shoot = 0;
 
-for (let x = 0 ; x < target.length ; x++){
-    target[x].addEventListener('click', function (){
-        if (turn < 19){
-            totalScore += x;
+for (let t = 0 ; t < target.length ; t++){
+    target[t].addEventListener('click', function (){
+        if (shoot < 19){
+            totalScore += t;
             console.log(totalScore);
-            turn++;
+            shoot++;
         }
         else {
-            totalScore += x;
+            totalScore += t;
             console.log('score = ' + totalScore);
         }
     })
