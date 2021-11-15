dd12d4bdc4769b8025bcc3a12213f615211f03f4
Sylvie-B <unsolo@hotmail.fr>
2021-11-15 11:36:25 +0100

    test if shoot < 8 else last shoot of 3 fly

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 let main = document.querySelector('main');
 let target = document.querySelector('.target').getElementsByTagName('div');
 console.log("target : " + target.length);
-let flyNbr = document.getElementById('flyNbr');
+let flyNbr = parseInt(document.getElementById('flyNbr').innerHTML);
+console.log(flyNbr);
 let fullScreen = document.querySelector('.fullScreen');
 let final = document.getElementById('final');
 let go = document.querySelector('.fa-long-arrow-alt-right');
@@ -10,19 +11,73 @@ main.style.width = innerWidth + "px";
 main.style.height = innerHeight + "px";
 
 let totalScore = 0;
-let shoot = 0;
 
-for (let t = 0 ; t < target.length ; t++){
-    target[t].addEventListener('click', function (){
-        if (shoot < 19){
-            totalScore += t;
-            console.log(totalScore);
-            shoot++;
+// shoot = 1 arrow, fly = 3 arrows
+// 1 session = 20 fly => totalScore max = 10 * 3 * 20 = 600 points
+
+let shoot = 0;
+let a1 = 0;
+let a2 = 0;
+let a3 = 0;
+let fly = 0;
+let total = 0;
+
+
+// user click on 3 value = display + ask validation
+// let test = shoot % 3;
+for(let i = 0 ; i < target.length ; i++){
+
+    // target[i].addEventListener('click', function (){
+    //     switch (test){
+    //         case 0 :
+    //             console.log("fleche 1");
+    //             break;
+    //         case 1 :
+    //             console.log("fleche 2");
+    //             break;
+    //         case 2 :
+    //             console.log("fleche 3");
+    //             console.log("score = xxx");
+    //             break;
+    //     }
+    //     test++;
+    //
+    // })
+
+    target[i].addEventListener('click', function (){
+        if (shoot < 8 ){
+            switch (shoot % 3){
+                case 0 :
+                    a1 = i;
+                    shoot++;
+                    break;
+                case 1 :
+                    a2 = i;
+                    shoot++;
+                    break;
+                case 2 :
+                    a3 = i;
+                    console.log(a1, a2, a3);
+
+                    fly = a1 + a2 + a3;
+                    total += fly;
+                    console.log("volet = " + fly);
+                    console.log("total = " + total);
+                    shoot++;
+                    flyNbr++;
+                    console.log("flyNbr = " + flyNbr);
+                    break;
+            }
+
         }
         else {
-            totalScore += t;
-            console.log('score = ' + totalScore);
+         // shoot = 9 - last shoot
+
+
         }
+
     })
 }
+
+
 
