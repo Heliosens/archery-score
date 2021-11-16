3c1ece24dc4cc2e11971af272660c2f39b76e6b1
Sylvie-B <unsolo@hotmail.fr>
2021-11-16 10:24:58 +0100

    run for 9 shoot = 3 fly

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 let main = document.querySelector('main');
 let target = document.querySelector('.target').getElementsByTagName('div');
-
+let userChoice = document.getElementById('userChoice');
 let flyNbr = parseInt(document.getElementById('flyNbr').innerHTML);
-console.log(flyNbr);
 let fullScreen = document.querySelector('.fullScreen');
 let final = document.getElementById('final');
 let go = document.querySelector('.fa-long-arrow-alt-right');
@@ -23,7 +22,6 @@ let plus = 0;
 let fly = 0; // score for a fly
 let currentScore = 0;
 
-// todo increase "10+" for target[i = 11]
 // user click on 3 value = display + ask validation
 // let test = shoot % 3;
 for(let i = 0 ; i < target.length ; i++){
@@ -34,45 +32,37 @@ for(let i = 0 ; i < target.length ; i++){
             case 0 :
                 if (i === 11){
                     plus++;
-                    a1 += 0;
+                    a1 = 10;
                 }
                 else {
                     a1 = i;
-                    shoot++;
                 }
-
-                console.log("volet = " + fly, "+ :" + plus, "score actuel : " + currentScore);
+                shoot++;
 
                 break;
             case 1 :
                 if (i === 11){
-                    a2 += 0;
+                    plus++;
+                    a2 = 10;
                 }
                 else {
                     a2 = i;
-                    shoot++;
                 }
-                console.log("volet = " + fly, "+ :" + plus, "score actuel : " + currentScore);
+                shoot++;
 
                 break;
             case 2 :
-                // if fly <= 20, or shoot <= 60
                 if(i === 11){
-                    a3 += 0;
+                    a3 = 10;
                     plus++;
                 }
                 else {
                     a3 = i;
-                    shoot++;
+
                 }
-
-                console.log(a1, a2, a3);
-
+                shoot++;
                 fly = a1 + a2 + a3;
                 currentScore += fly;
-                console.log("volet = " + fly, "+ :" + plus, "score actuel : " + currentScore);
-
-
 
                 if (shoot === 9){
                     console.log("TOTAL = " + currentScore);
