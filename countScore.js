8726a846ba2a85ebbda3875bcfbce97eebb86948
Sylvie-B <unsolo@hotmail.fr>
2022-02-05 20:37:12 +0100

    create count score object

---
+++ countScore.js
@@ -0,0 +1,22 @@
+let CountScore = function (nbrVolley, elem){
+    this.nbrVolley = nbrVolley;
+    this.elem = elem;
+    let shoot = 0;      // length of array volley
+    let volley = [];    // .push(a1 to a3);
+    let result = [];        // final array
+
+   let getPoint = function (a){
+       a = a === 11 ? "10+" : "a";
+       shoot = volley.push(a)
+   }
+
+   let getVolley = function (){
+       if (shoot === 3){
+
+       }
+   }
+
+   let totalScore = function (){
+
+   }
+}
