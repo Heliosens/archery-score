ee97a1c9a01606527f1866031448225ee4e43186
Sylvie-B <unsolo@hotmail.fr>
2022-02-08 14:52:17 +0100

    review table

--- FinalTable.js
+++ FinalTable.js
@@ -10,11 +10,11 @@ let FinalTable = function (elem, result){
     this.scoreTable = function (){
         let table = document.createElement('table');
         let thead = document.createElement('thead');
-        let tr = document.createElement("tr");
-        let cell = document.createElement("td");
-        cell.colSpan = 4;
-        cell.innerHTML = this.day.toLocaleDateString();
-        table.appendChild(thead).appendChild(tr).appendChild(cell);
+        let htr = document.createElement("tr");
+        let th = document.createElement("th");
+        th.colSpan = 4;
+        th.innerHTML = this.day.toLocaleDateString();
+        table.appendChild(thead).appendChild(htr).appendChild(th);
         for(let line of this.result){
             let value = 0;
             let shootVolley = document.createElement('tr');
@@ -30,6 +30,10 @@ let FinalTable = function (elem, result){
             shootVolley.appendChild(val);
             table.appendChild(shootVolley);
         }
+        let tfoot = document.createElement('tfoot');
+        let ftr = document.createElement('tr');
+        let td = document.createElement('td');
+        table.appendChild(tfoot).appendChild(ftr).appendChild(td);
         elem.appendChild(table);
     }
 // todo tbody, tfooter = total
